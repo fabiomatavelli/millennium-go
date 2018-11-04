4ad760a3207f2d33252e35c8102f47009ba7875c
Fábio Matavelli <fabiomatavelli@gmail.com>
2018-11-04 13:01:26 -0200

    Improve test coverage

--- millennium_test.go
+++ millennium_test.go
@@ -1,6 +1,7 @@
 package millennium
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -17,6 +18,15 @@ type mockHTTPServer struct {
 	testServer *httptest.Server
 }
 
+func (s *mockHTTPServer) jsonError(message string, errorCode int) []byte {
+	response := ResponseError{}
+
+	response.SetMessage(message)
+	response.SetCode(errorCode)
+
+	res, _ := json.Marshal(response)
+	return res
+}
 
 func (s *mockHTTPServer) Start() *httptest.Server {
 	mux := http.NewServeMux()
@@ -27,12 +37,48 @@ func (s *mockHTTPServer) Start() *httptest.Server {
 			w.Write([]byte(`{"session":"{00000000-0000-0000-0000-000000000000}"}`))
 		} else {
 			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte(`{"error":{"code":401,"message":{"lang":"en-us","value":"PERMISSÃO NEGADA:\r\rNão é possível autenticar o usuário. Senha inválida."}}}`))
+			w.Write(s.jsonError("PERMISSÃO NEGADA:\r\rNão é possível autenticar o usuário. Senha inválida.", 401))
 		}
 	})
 	mux.HandleFunc("/api/test.success.GET", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"odata.count": 1,"value":[{"number":1,"string":"test","bool":true}]}`))
+	})
+	mux.HandleFunc("/api/test.error400.GET", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(s.jsonError("Parameter not found", http.StatusBadRequest))
+	})
+	mux.HandleFunc("/api/test.error500.GET", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(s.jsonError("Query error", http.StatusInternalServerError))
+	})
+	mux.HandleFunc("/api/test.error.invalidjson", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"odata.count": 1,"value":["test":"test"}`))
+	})
+	mux.HandleFunc("/api/test.error.empty", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(``))
+	})
+	mux.HandleFunc("/api/test.success.POST", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"number":1,"string":"test","bool":true}`))
+	})
+	mux.HandleFunc("/api/test.error.POST", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(s.jsonError("Internal Server Error", http.StatusInternalServerError))
+	})
+	mux.HandleFunc("/api/test.success.DELETE", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"odata.metadata":""}`))
+	})
+	mux.HandleFunc("/api/test.error.DELETE", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(s.jsonError("Query error", http.StatusInternalServerError))
 	})
 
 	s.testServer = httptest.NewServer(mux)
@@ -121,6 +167,12 @@ func TestLogin(t *testing.T) {
 			AuthType:    Session,
 			ExpectError: false,
 		},
+		{
+			Username:    "test",
+			Password:    "test",
+			AuthType:    NTLM,
+			ExpectError: false,
+		},
 	}
 
 	for _, c := range cases {
@@ -141,8 +193,9 @@ func TestGet(t *testing.T) {
 	}
 
 	type Expect struct {
-		Error bool
-		Count int
+		Error    bool
+		Count    int
+		Response interface{}
 	}
 
 	var responseTestGET []ResponseTestGET
@@ -164,16 +217,121 @@ func TestGet(t *testing.T) {
 				Count: 1,
 			},
 		},
+		{
+			Method:   "test.error400.GET",
+			Response: &responseTestGET,
+			Params:   url.Values{},
+			Expect: Expect{
+				Error: true,
+				Count: 0,
+			},
+		},
+		{
+			Method:   "test.error500.GET",
+			Response: &responseTestGET,
+			Params:   url.Values{},
+			Expect: Expect{
+				Error: true,
+				Count: 0,
+			},
+		},
+		{
+			Method:   "test.error.invalidjson",
+			Response: &responseTestGET,
+			Params:   url.Values{},
+			Expect: Expect{
+				Error: true,
+				Count: 0,
+			},
+		},
+		{
+			Method:   "test.error.empty",
+			Response: &responseTestGET,
+			Params:   url.Values{},
+			Expect: Expect{
+				Error: true,
+				Count: 0,
+			},
+		},
 	}
 
 	for _, c := range cases {
-		count, err := client.Get(c.Method, c.Params, &responseTestGET)
+		count, err := client.Get(c.Method, c.Params, &c.Response)
 		if (err == nil) == c.Expect.Error {
 			t.Error(err)
 		}
 
 		if count != c.Expect.Count {
 			t.Errorf("Expected %v results but got %v", c.Expect.Count, count)
+		}
+	}
+}
+
+func TestPost(t *testing.T) {
+	client := NewClient(t)
+
+	type ResponseTestPOST struct {
+		Number int    `json:"number"`
+		String string `json:"string"`
+		Bool   bool   `json:"bool"`
+	}
+
+	cases := []struct {
+		Method      string
+		Body        []byte
+		ExpectError bool
+	}{
+		{
+			Method:      "test.success.POST",
+			Body:        []byte(`{"test":"test"}`),
+			ExpectError: false,
+		},
+		{
+			Method:      "test.error.POST",
+			Body:        []byte(`{"test":"test"}`),
+			ExpectError: true,
+		},
+	}
+
+	for _, c := range cases {
+		var res *ResponseTestPOST
+		err := client.Post(c.Method, c.Body, &res)
+		if (err == nil) == c.ExpectError {
+			t.Error(err)
+		}
+		_ = res
+	}
+}
+
+func TestDelete(t *testing.T) {
+	client := NewClient(t)
+
+	cases := []struct {
+		Method      string
+		Params      url.Values
+		Response    interface{}
+		ExpectError bool
+	}{
+		{
+			Method: "test.success.DELETE",
+			Params: url.Values{
+				"test": []string{"test"},
+			},
+			ExpectError: false,
+		},
+		{
+			Method: "test.error.DELETE",
+			Params: url.Values{
+				"test": []string{"test"},
+			},
+			ExpectError: true,
+		},
+	}
+
+	for _, c := range cases {
+		err := client.Delete(c.Method, c.Params)
+		if (err == nil) == c.ExpectError {
+			t.Error(err)
 		}
 	}
 }
